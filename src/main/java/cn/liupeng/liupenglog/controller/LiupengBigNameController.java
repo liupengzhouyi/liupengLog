e52263aa83d966778f96ae44ef6836a1157976b8
liupeng <liupeng.0@outlook.com>
2021-04-13 23:45:07 +0800

    图片管理器

---
+++ src/main/java/cn/liupeng/liupenglog/controller/LiupengBigNameController.java
@@ -0,0 +1,86 @@
+package cn.liupeng.liupenglog.controller;
+
+import cn.liupeng.liupenglog.entity.LiupengBigName;
+import cn.liupeng.liupenglog.service.LiupengBigNameService;
+import org.springframework.web.bind.annotation.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+import javax.annotation.Resource;
+
+import java.util.List;
+
+/**
+ * 称号表(LiupengBigName)表控制层
+ *
+ * @author liupeng
+ * @since 2021-04-13 23:48:55
+ */
+@Api(tags = {"管理称号"})
+@RestController
+@RequestMapping("liupengBigName")
+public class LiupengBigNameController {
+    /**
+     * 服务对象
+     */
+    @Resource
+    private LiupengBigNameService liupengBigNameService;
+
+    /**
+     * 通过主键查询单条数据
+     *
+     * @param id 主键
+     * @return 单条数据
+     */
+    @ApiOperation("查询一个称号")
+    @RequestMapping(path = "selectOne", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+    public LiupengBigName selectOne(Integer id) {
+        return this.liupengBigNameService.queryById(id);
+    }
+
+    /**
+     * 查询所有数据
+     *
+     * @return List
+     */
+    @ApiOperation("查询所有称号")
+    @RequestMapping(path = "selectAll", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+    public List<LiupengBigName> selectAll() {
+        return this.liupengBigNameService.queryAll();
+    }
+
+    /**
+     * 添加一条数据
+     *
+     * @return List
+     */
+    @ApiOperation("添加一个称号")
+    @RequestMapping(path = "addOne", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+    public LiupengBigName addOne(LiupengBigName liupengBigName) {
+        return this.liupengBigNameService.insert(liupengBigName);
+    }
+
+    /**
+     * 删除一条数据
+     *
+     * @return boolean
+     */
+    @ApiOperation("删除一个称号")
+    @RequestMapping(path = "delOne", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+    public boolean delOne(LiupengBigName liupengBigName) {
+        return this.liupengBigNameService.deleteById(liupengBigName.getId());
+    }
+
+    /**
+     * 删除一条数据
+     *
+     * @return LiupengBigName
+     */
+    @ApiOperation("更新数据称号")
+    @RequestMapping(path = "updateOne", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+    public LiupengBigName update(LiupengBigName liupengBigName) {
+        return this.liupengBigNameService.update(liupengBigName);
+    }
+
+
+}
---
+++ src/main/java/cn/liupeng/liupenglog/dao/LiupengBigNameDao.java
@@ -0,0 +1,84 @@
+package cn.liupeng.liupenglog.dao;
+
+import cn.liupeng.liupenglog.entity.LiupengBigName;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * 称号表(LiupengBigName)表数据库访问层
+ *
+ * @author liupeng
+ * @since 2021-04-13 23:48:54
+ */
+@Mapper
+public interface LiupengBigNameDao {
+
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param id 主键
+     * @return 实例对象
+     */
+    LiupengBigName queryById(Integer id);
+
+    /**
+     * 查询指定行数据
+     *
+     * @param offset 查询起始位置
+     * @param limit  查询条数
+     * @return 对象列表
+     */
+    List<LiupengBigName> queryAllByLimit(@Param("offset") int offset, @Param("limit") int limit);
+
+
+    /**
+     * 通过所有
+     *
+     * @return 对象列表
+     */
+    List<LiupengBigName> queryAll();
+
+    /**
+     * 新增数据
+     *
+     * @param liupengBigName 实例对象
+     * @return 影响行数
+     */
+    int insert(LiupengBigName liupengBigName);
+
+    /**
+     * 批量新增数据（MyBatis原生foreach方法）
+     *
+     * @param entities List<LiupengBigName> 实例对象列表
+     * @return 影响行数
+     */
+    int insertBatch(@Param("entities") List<LiupengBigName> entities);
+
+    /**
+     * 批量新增或按主键更新数据（MyBatis原生foreach方法）
+     *
+     * @param entities List<LiupengBigName> 实例对象列表
+     * @return 影响行数
+     */
+    int insertOrUpdateBatch(@Param("entities") List<LiupengBigName> entities);
+
+    /**
+     * 修改数据
+     *
+     * @param liupengBigName 实例对象
+     * @return 影响行数
+     */
+    int update(LiupengBigName liupengBigName);
+
+    /**
+     * 通过主键删除数据
+     *
+     * @param id 主键
+     * @return 影响行数
+     */
+    int deleteById(Integer id);
+
+}
+
