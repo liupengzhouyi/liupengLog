7d9909fe1279e45496cc296a25e5a202f0894d44
liupeng <liupeng.0@outlook.com>
2021-04-14 12:47:32 +0800

    review V 1.0.0

--- src/main/java/cn/liupeng/liupenglog/LiupengLogApplication.java
+++ src/main/java/cn/liupeng/liupenglog/LiupengLogApplication.java
@@ -3,6 +3,7 @@ package cn.liupeng.liupenglog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// strat
 @SpringBootApplication
 public class LiupengLogApplication {
 
